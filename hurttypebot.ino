fa805b9cc6b5229d94d144467649178f9ecbbee8
WLTjoa <55340911+WLTjoa@users.noreply.github.com>
2021-09-13 17:07:39 +0800

    Add files via upload

---
+++ hurttypebot.ino
@@ -0,0 +1,518 @@
+#include <Servo.h>
+
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;// create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo1.attach(9);
+  myservo2.attach(10);
+  myservo3.attach(11);// attaches the servo on pin 9 to the servo objecta
+  myservo1.write(0);
+  myservo2.write(0);
+  myservo3.write(0);
+  types();
+  typee();
+  typen();
+  typed();
+  space();
+  typeh();
+  typee();
+  typel();
+  typep();
+  enter();
+  typeq();
+  typew();
+  typeq();
+  enter();
+}
+
+
+//
+
+
+
+
+
+
+
+
+void loop() {  
+
+//    mysrvo.write(30);j              
+//    delay(1000);                      
+//    myservo.write(120);              
+//    delay(1000);                     
+
+}
+
+void rest(){
+    myservo1.write(100);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+    myservo3.write(85);
+    delay(1000);
+}
+
+void typeq() {
+    rest();
+    myservo1.write(105);
+    delay(1000);
+    myservo3.write(57);
+    delay(1000);
+    myservo2.write(90);
+    delay(200);
+    myservo2.write(120);
+    delay(200);
+
+}
+
+void typew() {
+    rest();
+    myservo1.write(90);
+    delay(1000);
+    myservo3.write(62);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void typee() {
+    rest();
+    myservo1.write(85);
+    delay(1000);
+    myservo3.write(72);
+    delay(1000);
+    myservo2.write(102);
+    delay(400);
+    myservo2.write(120);
+    delay(400);
+}
+
+void typer() {
+    rest();
+    myservo1.write(85);
+    delay(1000);
+    myservo3.write(85);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void typet() {
+    rest();
+    myservo1.write(80);
+    delay(1000);
+    myservo3.write(95);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void typey() {
+    rest();
+    myservo1.write(80);
+    delay(1000);
+    myservo3.write(105);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void typeu() {
+    rest();
+    myservo1.write(80);
+    delay(1000);
+    myservo3.write(115);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void typei() {
+    rest();
+    myservo1.write(82);
+    delay(1000);
+    myservo3.write(127);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void typeo() {
+    rest();
+    myservo1.write(85);
+    delay(1000);
+    myservo3.write(135);
+    delay(1000);
+    myservo2.write(100);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+
+void typep() {
+    rest();
+    myservo1.write(86);
+    delay(1000);
+    myservo3.write(135);
+    delay(1000);
+    myservo2.write(105);
+    delay(300);
+    myservo2.write(120);
+    delay(300);
+}
+
+void typea() {
+    rest();
+    myservo1.write(90);
+    delay(1000);
+    myservo3.write(55);
+    delay(1000);
+    myservo2.write(105);
+    delay(100);
+    myservo2.write(120);
+    delay(100);
+}
+
+void types() {
+    rest();
+    myservo1.write(80);
+    delay(1000);
+    myservo3.write(65);
+    delay(1000);
+    myservo2.write(105);
+    delay(500);
+    myservo2.write(120);
+    delay(500);
+}
+
+void typed() {
+    rest();
+    myservo1.write(75);
+    delay(1000);
+    myservo3.write(70);
+    delay(1000);
+    myservo2.write(102);
+    delay(500);
+    myservo2.write(120);
+    delay(500);
+}
+
+
+
+void typef() {
+//    rest();
+//    myservo1.write(78);
+//    delay(1000);
+//    myservo3.write(85);
+//    delay(1000);
+//    myservo2.write(100);
+//    delay(500);
+//    myservo2.write(120);
+//    delay(100);
+    rest();
+    myservo3.write(85);
+    delay(1000);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(95);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+
+void typeg() {
+//    rest();
+//    myservo1.write(80);
+//    delay(1000);
+//    myservo3.write(100);
+//    delay(1000);
+//    myservo2.write(100);
+//    delay(200);
+//    myservo2.write(120);
+//    delay(100);
+    rest();
+    myservo3.write(105);
+    delay(1000);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(95);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typeh() {
+//    rest();
+//    myservo1.write(78);
+//    delay(1000);
+//    myservo3.write(113);
+//    delay(1000);
+//    myservo2.write(95);
+//    delay(100);
+//    myservo2.write(120);
+//    delay(100); 
+    rest();
+    myservo3.write(115);
+    delay(1000);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(95);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typej() {
+    rest();
+    myservo1.write(78);
+    delay(1000);
+    myservo3.write(125);
+    delay(1000);
+    myservo2.write(95);
+    delay(100);
+    myservo2.write(140);
+    delay(100);
+    myservo1.write(78);
+    delay(1000);
+}
+
+void typek() {
+    rest();
+    myservo1.write(80);
+    delay(1000);
+    myservo3.write(135);
+    delay(1000);
+    myservo2.write(98);
+    delay(100);
+    myservo2.write(140);
+    delay(100);
+    myservo1.write(78);
+    delay(1000);
+}
+
+
+void typel() {
+    rest();
+    myservo1.write(73);
+    delay(1000);
+    myservo3.write(135);
+    delay(1000);
+    myservo2.write(95);
+    delay(300);
+    myservo2.write(140);
+    delay(300);
+    myservo1.write(78);
+    delay(1000);
+}
+//
+
+void typez() {
+    rest();
+    myservo1.write(85);
+    delay(1000);
+    myservo3.write(55);
+    delay(1000);
+    myservo2.write(98);
+    delay(100);
+    myservo2.write(140);
+    delay(100);
+    myservo1.write(78);
+    delay(1000);
+}
+
+void typex() {
+    rest();
+    myservo3.write(70);
+    delay(1000);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(95);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(60);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typec() {
+    rest();
+    myservo3.write(80);
+    delay(1000);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(95);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typev() {
+    rest();
+    myservo3.write(95);
+    delay(1000);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(90);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typeb() {
+    rest();
+    myservo3.write(115);
+    delay(1000);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(80);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typen() {
+    rest();
+    myservo3.write(125);
+    delay(1000);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(90);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void typem() {
+    rest();
+    myservo3.write(145);
+    delay(1000);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(80);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void space() {
+    rest();
+    myservo3.write(95);
+    delay(1000);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(80);
+    delay(500);
+    myservo1.write(90);
+    delay(500);
+    myservo1.write(50);
+    delay(1000);
+    myservo2.write(120);
+    delay(1000);
+}
+
+void enter() {
+    rest();
+    myservo3.write(157);
+    delay(1000);
+    myservo1.write(105);
+    delay(1000);
+    myservo2.write(100);
+    delay(200);
+    myservo2.write(140);
+    delay(200);
+    myservo1.write(78);
+    delay(1000);
+}
+//---------------------------
+
+
+
+
+
+
+void type3() {
+    rest();
+    myservo1.write(15);
+    delay(1000);
+    myservo3.write(70);
+    delay(1000);
+    myservo2.write(90);
+    delay(100);
+    rest();
+}
+
+void type4() {
+    rest();
+    myservo1.write(3);
+    delay(1000);
+    myservo3.write(80);
+    delay(1000);
+    myservo2.write(90);
+    delay(100);
+    rest();
+}
+
+void type5() {
+    rest();
+    myservo1.write(3);
+    delay(1000);
+    myservo3.write(85);
+    delay(1000);
+    myservo2.write(90);
+    delay(100);
+    rest();
+}
